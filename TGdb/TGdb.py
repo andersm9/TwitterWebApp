8f677e9b53c1922b2fae7d9e7521f7eae621a905
andersm9 <m_anderson9@YAHOO.CO.UK>
2017-09-06 09:29:02 +0100

    changed track term

--- TGdb/TGdb.py
+++ TGdb/TGdb.py
@@ -120,7 +120,7 @@ def Database_write2(tweet,polarity,magnitude, lat, long, user_name):
 ## start here
 
 if __name__ == "__main__":
-    TRACK_TERM = 'VMB, Virgin Media Business'
+    TRACK_TERM = 'pizza'
     print("Twitter 1 - try to connect")
     r = api.request('statuses/filter', {'track': TRACK_TERM})
     print("connected, awaiting tweet")
